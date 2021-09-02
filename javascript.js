e43611950dd7ba755d22036565d7a92c40a25981
Nashtarin Nur <nashtarin.nur@gmail.com>
2021-09-02 14:07:39 +0600

    complete

---
+++ javascript.js
@@ -0,0 +1,58 @@
+// Loading data api
+
+
+
+const loadBook = () => {
+    const searchId = document.getElementById("searchText");
+    const searchInput = searchId.value;
+    searchId.value = '';
+
+    const url = `https://openlibrary.org/search.json?q=${searchInput}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => docs(data.docs));
+}
+const docs = books => {
+    const bookSearchResult = document.getElementById("bookSearchResult");
+    bookSearchResult.textContent = '';
+
+    if (books.length === 0) {
+        bookSearchResult.innerHTML = `<h2>No Result found!<h2>`
+
+    }
+
+
+
+    books.forEach(book => {
+        console.log(book);
+
+
+
+        const div = document.createElement('div');
+        div.classList.add("col-md-6");
+        div.classList.add("col-sm-12");
+        div.innerHTML = `
+        
+                    <div class="shadow p-3card border-primary h-100 rounded-3">
+                         <img class=" rounded-3 mx-auto w-100 p-4 rounded-3" src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                        <h4 class="fs-2 card-title">${book.title}</h4>
+                        <p class="fw-bold card-text">Author Name:${book.author_name[0]}</p>
+                        <p>Publisher:${book.publisher[0]}</p>
+                        <p>First Publish Date:${book.publish_date[0]}</p>
+                        <p>First Publish Year:${book.publish_year[0]}</p>
+                        
+                        </div>
+                    </div>
+                `
+
+        bookSearchResult.appendChild(div);
+
+    });
+
+}
+
+
+
+
+
